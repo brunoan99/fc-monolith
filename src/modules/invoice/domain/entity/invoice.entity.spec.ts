a65a93732583b5eaf3998a9d3754c93548992b80
Bruno Andrade <brunoan99@gmail.com>
2022-10-20 19:48:43 -0300

    feat(invoice): add Invoice Entity implementation and tests

---
+++ src/modules/invoice/domain/entity/invoice.entity.spec.ts
@@ -0,0 +1,63 @@
+import { Address } from '../value-object/address.value-object'
+import { Invoice } from './invoice.entity'
+import { Product } from './product.entity'
+
+const makeValidItems = () => {
+  return [
+    new Product({
+      name: 'Product1',
+      price: 10
+    }),
+    new Product({
+      name: 'Product2',
+      price: 20
+    }),
+    new Product({
+      name: 'Product3',
+      price: 30
+    }),
+  ]
+}
+
+const makeValidAddress = () => {
+  return new Address({
+    street: 'street',
+    number: 'number',
+    complement: 'complement',
+    city: 'city',
+    state: 'state',
+    zipCode: 'zipCode',
+  })
+}
+
+const makeValidInput = () => ({
+  document: 'any document',
+  address: makeValidAddress(),
+  items: makeValidItems()
+})
+
+describe('Product Entity', () => {
+  test('Should return an error if provided document is a empty string', () => {
+    const input = makeValidInput()
+    input.document = ''
+    expect(() => {new Invoice(input)}).toThrowError('Document is required')
+  })
+
+  test('Should return an error if provided address is undefined', () => {
+    const input = makeValidInput()
+    input.address = undefined
+    expect(() => {new Invoice(input)}).toThrowError('Address is required')
+  })
+
+  test('Should return an error if items is empty', () => {
+    const input = makeValidInput()
+    input.items = []
+    expect(() => {new Invoice(input)}).toThrowError('Items must contain at least one item')
+  })
+
+  test('Should return an invoice with generated id', () => {
+    const input = makeValidInput()
+    const output = new Invoice(input)
+    expect(output.id).toBeDefined()
+  })
+})
---
+++ src/modules/invoice/domain/entity/invoice.entity.ts
@@ -0,0 +1,46 @@
+import { AggregateRoot } from '../../../@shared/domain/entity/aggregate-root.interface'
+import { Entity } from '../../../@shared/domain/entity/base.entity'
+import { Id } from '../../../@shared/domain/value-object/id.value-object'
+import { Address } from '../value-object/address.value-object'
+import { Product } from './product.entity'
+
+type InvoiceProps = {
+  id?: string
+  createdAt?: Date
+  updatedAt?: Date
+  document: string
+  address: Address
+  items: Product[]
+}
+
+export class Invoice extends Entity implements AggregateRoot {
+  private _document: string
+  private _address: Address
+  private _items: Product[]
+
+  constructor(props: InvoiceProps) {
+    super({ id: new Id(props.id), createdAt: props.createdAt, updatedAt: props.updatedAt })
+    this._document = props.document
+    this._address = props.address
+    this._items = props.items
+    this.validate()
+  }
+
+  get document(): string { return this._document }
+
+  get address(): Address { return this._address }
+
+  get items(): Product[] { return this._items }
+
+  validate() {
+    if (this._document === '') {
+      throw new Error('Document is required')
+    }
+    if (this._address === undefined ) {
+      throw new Error('Address is required')
+    }
+    if (this._items.length === 0) {
+      throw new Error('Items must contain at least one item')
+    }
+  }
+}
