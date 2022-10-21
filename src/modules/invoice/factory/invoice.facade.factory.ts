e5de48d0bab132cc98f16261648ab512fbc6c777
Bruno Andrade <brunoan99@gmail.com>
2022-10-20 22:05:10 -0300

    feat(invoice): add Invoice Facade Factory

---
+++ src/modules/invoice/factory/invoice.facade.factory.ts
@@ -0,0 +1,17 @@
+import { InvoiceFacade } from '../facade/invoice.facade'
+import { InvoiceRepository } from '../repository/invoice.repository'
+import { FindInvoiceUseCase } from '../usecase/find-invoice/find-invoice.usecase'
+import { GenerateInvoiceUseCase } from '../usecase/generate-invoice/generate-invoice.usecase'
+
+export class PaymentFacadeFactory {
+  static create() {
+    const invoiceRepo = new InvoiceRepository()
+    const generateInvoiceUseCase = new GenerateInvoiceUseCase(invoiceRepo)
+    const findInvoiceUseCase = new FindInvoiceUseCase(invoiceRepo)
+    const invoiceFacade = new InvoiceFacade(
+      generateInvoiceUseCase,
+      findInvoiceUseCase
+    )
+    return invoiceFacade
+  }
+}
